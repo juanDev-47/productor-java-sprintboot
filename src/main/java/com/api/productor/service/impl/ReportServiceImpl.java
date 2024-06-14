9a811aa5b7696305e168c1a04b3ab552faae00bb
juanDev-47 <juan1037av@gmail.com>
2024-06-13 21:42:27 -0500

    new: implement maker reports, with name, country and description

--- src/main/java/com/api/productor/service/impl/ReportServiceImpl.java
+++ src/main/java/com/api/productor/service/impl/ReportServiceImpl.java
@@ -1,68 +1,81 @@
 package com.api.productor.service.impl;
 
+import com.api.productor.controllers.dto.MakerDTO;
+import com.api.productor.service.IMakerService;
 import com.api.productor.service.IReportService;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 @Service
 public class ReportServiceImpl implements IReportService {
+
+    @Autowired
+    private IMakerService makerService;
 
 
     @Override
     public String createBook(String name) throws IOException {
 
+        // get all the makers
+        List<MakerDTO> makers = getMakers();
+
 //        1) create book
-        Workbook book = new XSSFWorkbook();
+        XSSFWorkbook book = new XSSFWorkbook();
 
         // 2) create sheets
-        Sheet hoja = book.createSheet("informe 1");
+        XSSFSheet hoja = book.createSheet("informe 1");
 
-        // create a bucle to iterate over the row, example 10 rows
-//        for (int i = 0; i < 10; i++) {
-//            Row fila = hoja.createRow(i);
-//            for (int j = 0; j < 10; j++) {
-//                Cell celda = fila.createCell(j);
-//                celda.setCellValue("Fila " + i + " Columna " + j);
-//            }
-//        }
-        // 3) create rows
-        Row cabecera = hoja.createRow(2);
-        // 4) create columns
-        Cell nombre = cabecera.createCell(1);
-        Cell edad = cabecera.createCell(2);
-        Cell ciudad = cabecera.createCell(3);
+        // create the header
+        XSSFRow cabecera = hoja.createRow(0);
+        XSSFCell celda1 = cabecera.createCell(1);
+        XSSFCell celda2 = cabecera.createCell(2);
+        XSSFCell celda3 = cabecera.createCell(3);
 
-        nombre.setCellValue("Nombre");
-        edad.setCellValue("Edad");
-        ciudad.setCellValue("Ciudad");
+        // create cell styles
+        XSSFCellStyle style = book.createCellStyle();
 
-        Row fila1 = hoja.createRow(3);
-        Cell nombre1 = fila1.createCell(1);
-        Cell edad1 = fila1.createCell(2);
-        Cell ciudad1 = fila1.createCell(3);
+        // config styles
+        style.setFillForegroundColor(IndexedColors.LAVENDER.getIndex());
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        style.setBorderBottom(BorderStyle.THIN);
+        style.setBorderTop(BorderStyle.THIN);
+        style.setBorderLeft(BorderStyle.THIN);
+        style.setBorderRight(BorderStyle.THIN);
 
-        nombre1.setCellValue("Juan");
-        edad1.setCellValue(30);
-        ciudad1.setCellValue("Medellin");
+        // config cel
+        celda1.setCellValue("Full Name");
+        celda1.setCellStyle(style);
+        celda2.setCellValue("Country");
+        celda2.setCellStyle(style);
+        celda3.setCellValue("Description");
+        celda3.setCellStyle(style);
 
-        Row fila2 = hoja.createRow(4);
-        Cell nombre2 = fila2.createCell(1);
-        Cell edad2 = fila2.createCell(2);
-        Cell ciudad2 = fila2.createCell(3);
+        hoja.autoSizeColumn(1);
+        hoja.autoSizeColumn(2);
+        hoja.autoSizeColumn(3);
 
-        nombre2.setCellValue("Linda K. Orellana D. A.");
-        edad2.setCellValue(30);
-        ciudad2.setCellValue("Medellin");
+        // create a bucle to iterate over the makers list
+        for (int i = 1; i < makers.size(); i++) {
+            XSSFRow fila = hoja.createRow(i);
+            XSSFCell cel1 = fila.createCell(1);
+            cel1.setCellStyle(style);
+            XSSFCell cel2 = fila.createCell(2);
+            cel2.setCellStyle(style);
+            XSSFCell cel3 = fila.createCell(3);
+            cel3.setCellStyle(style);
 
+            cel1.setCellValue(makers.get(i).getFullName());
+            cel2.setCellValue(makers.get(i).getCountry());
+            cel3.setCellValue(makers.get(i).getDescription());
+        }
 
 
         try {
@@ -90,6 +103,16 @@ public class ReportServiceImpl implements IReportService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public List<MakerDTO> getMakers() {
+        return makerService.findAll().stream().map(maker -> MakerDTO.builder()
+                .id(maker.getId())
+                .fullName(maker.getFullName())
+                .country(maker.getCountry())
+                .description(maker.getDescription())
+                .products(maker.getProducts())
+                .build()).toList();
     }
 
 
